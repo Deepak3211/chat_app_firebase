4981a62a7f848e524f251eae99b6ab30032d9189
deepak3211 <deepakt754@gmail.com>
2021-04-22 01:24:14 +0530

    changing some css

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -4,34 +4,43 @@ import { useStateValue } from '../../context/StateProvider'
 import './Auth.css'
 import logo from './img/google-login-button.png'
 const Auth = () => {
-  const [{ user}, dispatch] = useStateValue();
-  const signIn = e =>{
-    // console.log('😆')
-    e.preventDefault()
-    auth.signInWithPopup(provider)
-      .then(result => {
-        // console.log(result)
-        dispatch({
-          type: SET_USER,
-          user: {
-            email: result.user.email,
-						uid: result.user.uid,
-						displayName: result.user.displayName,
-						photoURL: result.user.photoURL,
-          }
-        })
-        
-      })
-      .catch(error=> {
-      console.log(error);
-      })
-    }
-   
-  return (
-    <div className="auth">
-      <img className="auth__logo" onClick = {signIn} src={logo} alt="logo"/>
-    </div>
-  )
+const [{ user}, dispatch] = useStateValue();
+const signIn = e =>{
+// console.log('😆')
+e.preventDefault()
+auth.signInWithPopup(provider)
+.then(result => {
+// console.log(result)
+if (result) {
+
+dispatch({
+type: SET_USER,
+user: {
+email: result.user.email,
+uid: result.user.uid,
+displayName: result.user.displayName,
+photoURL: result.user.photoURL,
+}
+})
+}
+else {
+dispatch({
+type: SET_USER,
+user: null,
+})
+}
+
+})
+.catch(error=> {
+console.log(error);
+})
+}
+
+return (
+<div className="auth">
+<img className="auth__logo" onClick = {signIn} src={logo} alt="logo"/>
+</div>
+)
 }
 
 export default Auth
--- src/components/chatSection/Message.js
+++ src/components/chatSection/Message.js
@@ -2,9 +2,9 @@ import { useStateValue } from '../../context/StateProvider'
 import './Message.css'
 const Message = ({ message, timestamp, photoURL,email }) => {
 const [{ user }] = useStateValue();
-// const msgClass = name === user.displayName ? 'send' : 'received'
+const msgClass = email === user.email ? 'send' : 'received'
 return (
-<div className={`chat__message ${email === user.email &&'chat__receiver'}`}>
+<div className={`chat__message ${msgClass}`}>
 
 <img className = 'profilepic' src={photoURL } alt = 'profilepic' />
 {/* <p className = 'chat__username'>{name}</p> */}
